9126d764d13fafe42029507d76c87a4f857e378b
creepochek <alexei.chizhov2010@gmail.com>
2023-12-13 19:37:08 +0200

    1234

--- .hello.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/o-chyzhov-io14/test3/hello.ko := ccache arm-linux-gnueabihf-ld -r  -EL -z noexecstack  -T ./scripts/module-common.lds -T ./arch/arm/kernel/module.lds  --build-id  -o /home/o-chyzhov-io14/test3/hello.ko /home/o-chyzhov-io14/test3/hello.o /home/o-chyzhov-io14/test3/hello.mod.o ;  true
--- .tmp_versions/hello.mod
+++
@@ -1,3 +0,0 @@
-/home/o-chyzhov-io14/test3/hello.ko
-/home/o-chyzhov-io14/test3/hello.o
-
